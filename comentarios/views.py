6a9f6e050ec4b8cec3f650597e85ee37f052003b
Sebastián Catalán <sicatalan@uc.cl>
2022-08-27 19:00:56 -0400

    connect data from different apps

--- comentarios/views.py
+++ comentarios/views.py
@@ -1,3 +1,6 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def test(request):
+    return HttpResponse("Funciona correctamente")
